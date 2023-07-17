d94ed3c020153c5787833e5efa15b464cf029f4a
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2023-07-17 12:57:07 -0700

    created 3 movie data entries

---
+++ PE02 - Movie List/movie-list/src/movie-data.js
@@ -0,0 +1,16 @@
+export const movies = [{
+    id: 0,
+    title: "Grease",
+    genre: "Musical",
+    year: "1978"
+},{
+    id: 1,
+    title: "Dude, Where's My Car?",
+    genre: "Comedy",
+    year: "2000",
+},{
+    id: 2,
+    title: "War of the Worlds",
+    genre: "Sci-Fi",
+    year: "2005"
+}]
