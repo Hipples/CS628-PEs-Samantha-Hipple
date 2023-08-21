783e0d0285a5186c89161e25155e7e083f8fe220
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2023-08-21 08:54:54 -0700

    updated server.mjs

--- PE05 - Recipe Finder/backend/server.mjs
+++ PE05 - Recipe Finder/backend/server.mjs
@@ -1,16 +1,10 @@
-// express creates the web server
 import express from 'express';
-// cors middleware enables cross-origin resource sharing
 import cors from 'cors';
 
-// custom import to load environment variables
 import './load_environment.mjs';
-// custom import to load the recipes router
 import recipes from './routes/recipes.mjs';
 
-// set the server port 
 const PORT = process.env.PORT || 3000;
-// set an instance of an express application
 const app = express();
 
 // use the cors middleware to enable cross-origin resource sharing for all routes
@@ -20,7 +14,7 @@ app.use(express.json());
 // use the recipes router for any requests to the "/recipe" endpoint
 app.use("/recipe", recipes);
 
-// the root path ("/") sends a welcome message as the response
+// the root path sends a welcome message as the response
 app.get("/", (req, res) =>{
   res.send("Welcome to the Recipe Finder API!");
 });
