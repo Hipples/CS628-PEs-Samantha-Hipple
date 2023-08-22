e3833674267b8e97a01236f5e851d0f4e57d9926
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2023-08-21 17:15:11 -0700

    updated RecipeList

--- PE05 - Recipe Finder/frontend/src/App.js
+++ PE05 - Recipe Finder/frontend/src/App.js
@@ -1,27 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import './services/api';
+import './App.css'
 
+import useRecipes from './hooks/use-recipes'
 
-function App() {
+const App = () => {
+  const { recipes, loading, error } = useRecipes();
+
+  loading ? <div></div> 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
   );
 }
-
-export default App;
